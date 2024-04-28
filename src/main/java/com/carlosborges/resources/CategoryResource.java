fa7b4c2eb5ed23be19f5a59b965349ac76bbfef2
Carlos Borges <kaca_master@hotail.com>
2024-04-28 20:50:13 -0300

    Category created

--- src/main/java/com/carlosborges/resources/CategoryResource.java
+++ src/main/java/com/carlosborges/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package com.carlosborges.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carlosborges.entities.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST esta funcionando";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 
