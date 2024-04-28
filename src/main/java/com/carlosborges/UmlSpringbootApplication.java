f0d20a7e01db0de9580f99759b3a8706ab2ea4c0
Carlos Borges <kaca_master@hotail.com>
2024-04-28 20:03:25 -0300

    Project created

---
+++ src/main/java/com/carlosborges/UmlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.carlosborges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlSpringbootApplication.class, args);
+	}
+
+}
