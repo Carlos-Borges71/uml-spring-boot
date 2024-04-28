fad62eb43994c5a810297a531c279100846386b8
Carlos Borges <kaca_master@hotail.com>
2024-04-28 20:23:33 -0300

    REST Funcionation

---
+++ src/main/java/com/carlosborges/resources/CategoryResource.java
@@ -0,0 +1,17 @@
+package com.carlosborges.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	 
+	@RequestMapping(method = RequestMethod.GET)
+	public String list() {
+		return "REST esta funcionando";
+	}
+	
+
+}
