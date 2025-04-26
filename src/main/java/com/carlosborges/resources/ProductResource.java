95226dfeb838d5e3c046360c07082fe5181c30e7
Carlos Borges <161647223+Carlos-Borges71@users.noreply.github.com>
2025-04-25 23:28:39 -0300

    Update ProductResource.java

--- src/main/java/com/carlosborges/resources/ProductResource.java
+++ src/main/java/com/carlosborges/resources/ProductResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.carlosborges.entities.Product;
 import com.carlosborges.services.ProductService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
